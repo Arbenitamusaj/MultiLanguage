32c37ebb5eef1cb46733e0e492d49c1f7ec441d4
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2024-06-06 10:33:37 +0200

    Add project files.

---
+++ MultiLanguageExamManagementSystem/Models/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiLanguageExamManagementSystem.Models.Entities
+{
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+    }
+}
---
+++ MultiLanguageExamManagementSystem/Models/Entities/Language.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MultiLanguageExamManagementSystem.Models.Entities
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LanguageCode { get; set; }
+
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
